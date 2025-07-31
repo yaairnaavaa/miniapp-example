0b5569ed1f93d6e9293d313ef0622cb1d555845c
Yair Nava <iryanavahe@ittepic.edu.mx>
2025-07-31 00:55:41 -0700

    test verify

--- src/app/(protected)/home/page.tsx
+++ src/app/(protected)/home/page.tsx
@@ -1,13 +1,14 @@
 "use client";
+
+import { useEffect, useState } from 'react';
 import { Page } from '@/components/PageLayout';
 import { UserInfo } from '@/components/UserInfo';
 import { Verify } from '@/components/Verify';
 import { Guestbook } from '@/components/Guesbook';
 import { Marble, TopBar } from '@worldcoin/mini-apps-ui-kit-react';
-import { useEffect, useState } from 'react';
 
-export default async function Home() {
-  const [session, setSession] = useState<any>(null); // tipa mejor si puedes
+export default function Home() {
+  const [session, setSession] = useState<any>(null); // Te sugiero tiparlo si sabes la forma de session
   const [isVerify, setIsVerify] = useState<boolean>(false);
 
   useEffect(() => {
@@ -17,26 +18,28 @@ export default async function Home() {
     };
     fetchAuth();
   }, []);
-  
+
   return (
     <>
       <Page.Header className="p-0">
         <TopBar
           title="Home"
           endAdornment={
-            <div className="flex items-center gap-2">
-              <p className="text-sm font-semibold capitalize">
-                {session?.user.username}
-              </p>
-              <Marble src={session?.user.profilePictureUrl} className="w-12" />
-            </div>
+            session?.user && (
+              <div className="flex items-center gap-2">
+                <p className="text-sm font-semibold capitalize">
+                  {session.user.username}
+                </p>
+                <Marble src={session.user.profilePictureUrl} className="w-12" />
+              </div>
+            )
           }
         />
       </Page.Header>
       <Page.Main className="flex flex-col items-center justify-start gap-4 mb-16">
         <UserInfo />
-        <Verify onVerified={setIsVerify}/>
-        <Guestbook isVerify={isVerify}/>
+        <Verify onVerified={setIsVerify} />
+        <Guestbook isVerify={isVerify} />
       </Page.Main>
     </>
   );
