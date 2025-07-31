ef2f8c6a06f073aebb62036863cbd07a1c2d1e1e
Yair Nava <iryanavahe@ittepic.edu.mx>
2025-07-31 00:10:37 -0700

    test verify

--- src/app/(protected)/home/page.tsx
+++ src/app/(protected)/home/page.tsx
@@ -1,10 +1,7 @@
 import { auth } from '@/auth';
 import { Page } from '@/components/PageLayout';
-import { Pay } from '@/components/Pay';
-import { Transaction } from '@/components/Transaction';
 import { UserInfo } from '@/components/UserInfo';
 import { Verify } from '@/components/Verify';
-import { ViewPermissions } from '@/components/ViewPermissions';
 import { Marble, TopBar } from '@worldcoin/mini-apps-ui-kit-react';
 
 export default async function Home() {
